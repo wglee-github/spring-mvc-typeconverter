915cefd05f622ed677befa7c6b50d1b98dd6f9eb
wglee <amd0223@gmail.com>
2023-07-06 23:04:06 +0900

    섹션 10. 스프링 타입 컨버터 7.포맷터 - Formatter 8.포맷터를 지원하는 컨버전 서비스 9.포맷터 적용하기 10.스프링이 제공하는 기본 포맷터

--- src/main/java/hello/typeconverter/WebConfig.java
+++ src/main/java/hello/typeconverter/WebConfig.java
@@ -6,6 +6,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import hello.typeconverter.converter.IntegerToStringConverter;
 import hello.typeconverter.converter.IpPortToStringConverter;
+import hello.typeconverter.converter.MyNumberFormatter;
 import hello.typeconverter.converter.StringToIntegerConverter;
 import hello.typeconverter.converter.StringToIpPortConverter;
 
@@ -14,10 +15,13 @@ public class WebConfig implements WebMvcConfigurer{
 
 	@Override
 	public void addFormatters(FormatterRegistry registry) {
-		registry.addConverter(new IntegerToStringConverter());
-		registry.addConverter(new StringToIntegerConverter());
+		// 컨버터가 포맷터보다 우선순위 높기때문에 같은 기능을 하는 컨버터 주석처리  
+//		registry.addConverter(new IntegerToStringConverter());
+//		registry.addConverter(new StringToIntegerConverter());
 		registry.addConverter(new IpPortToStringConverter());
 		registry.addConverter(new StringToIpPortConverter());
+		
+		registry.addFormatter(new MyNumberFormatter());
 	}
 
 	
--- src/main/java/hello/typeconverter/controller/ConverterController.java
+++ src/main/java/hello/typeconverter/controller/ConverterController.java
@@ -1,10 +1,5 @@
 package hello.typeconverter.controller;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -29,24 +24,6 @@ public class ConverterController {
 		IpPort ipPort = new IpPort("127.0.0.1", 8080);
 		Form form = new Form(ipPort, "사용자");
 		model.addAttribute("form", form);
-		
-		List<Form> list1 = new ArrayList<>();
-		list1.add(new Form(new IpPort("127.0.0.1", 8081), "A사용자"));
-		list1.add(new Form(new IpPort("127.0.0.2", 8082), "B사용자"));
-		List<Form> list2 = new ArrayList<>();
-		list2.add(new Form(new IpPort("127.0.0.3", 8083), "C사용자"));
-		list2.add(new Form(new IpPort("127.0.0.4", 8084), "D사용자"));
-		List<Form> list3 = new ArrayList<>();
-		list3.add(new Form(new IpPort("127.0.0.5", 8085), "E사용자"));
-		list3.add(new Form(new IpPort("127.0.0.6", 8085), "F사용자"));
-		
-		Map<String, List<Form>> map = new HashMap<>();
-		map.put("A", list1);
-		map.put("B", list2);
-		map.put("C", list3);
-		
-		model.addAttribute("map", map);
-		
 		return "converter-form";
 	}
 	
